c6d0182b1a10f39ed2f5eff8ebfd88d4fd0ff038
develar <develar@gmail.com>
2018-03-07 15:58:41 +0100

    CopyFile function

--- fs.go
+++ fs.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"path/filepath"
 	"github.com/develar/errors"
+	"io"
 )
 
 // Creates the named file and parent directories if need
@@ -30,6 +31,41 @@ func CreateFile(name string) (*os.File, error) {
 	return file, nil
 }
 
+func CopyFile(from string, to string, fromInfo os.FileInfo) error {
+	sourceFile, err := os.Open(from)
+	if err != nil {
+		return errors.WithStack(err)
+	}
 
+	defer sourceFile.Close()
+	return WriteFile(sourceFile, to, fromInfo)
+}
 
+func WriteFile(source io.Reader, to string, fromInfo os.FileInfo) error {
+	// cannot use file mode as is because of *** *** *** umask
+	destinationFile, err := os.OpenFile(to, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		return errors.WithStack(err)
+	}
 
+	_, err = io.Copy(destinationFile, source)
+	if err != nil {
+		destinationFile.Close()
+		return errors.WithStack(err)
+	}
+
+	perm := fromInfo.Mode().Perm()
+	if perm != 0644 {
+		err = os.Chmod(to, perm)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+	}
+
+	err = destinationFile.Close()
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	return nil
+}
